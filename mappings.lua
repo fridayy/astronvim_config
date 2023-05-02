6439f7cc04a71c34a90525cdc127c0cdd1617658
Benjamin Krenn <benjamin.krenn@leftshift.one>
2023-05-02 13:19:57 +0200

    changes

--- mappings.lua
+++ mappings.lua
@@ -9,6 +9,10 @@ return {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>bb"] = { function()
+      local builtin = require('telescope.builtin')
+      builtin.buffers()
+    end, desc = "Buffer picker"},
     ["<leader>bD"] = {
       function()
         require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
